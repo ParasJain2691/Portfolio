d97b3a679087ffa6dfc3c22eb9752e2a40c1cff2
ParasJain2691 <83552830+ParasJain2691@users.noreply.github.com>
2023-05-01 18:56:27 +0000

    Addes pages

--- app.py
+++ app.py
@@ -8,5 +8,25 @@ def hello_world():
   return render_template('home.html')
 
 
+@app.route("/experience")
+def experience():
+  return render_template("experience.html")
+
+
+@app.route("/education")
+def education():
+  return render_template("education.html")
+
+
+@app.route("/certificates")
+def certificates():
+  return render_template("certificates.html")
+
+
+@app.route("/skills")
+def skills():
+  return render_template("skills.html")
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
